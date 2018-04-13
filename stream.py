e72f5a7d855aa276df68bc9adb1e82fb12708476
Ankit Mishra <mishra14@gmail.com>
2018-04-13 05:06:21 +0000

    Adding support for multiple  cameras

--- stream.py
+++ stream.py
@@ -1,3 +1,4 @@
+from multiprocessing import Process
 import os
 import pygame.camera
 import pygame.image
@@ -6,24 +7,11 @@ import thread
 
 class Camera:
 
-    def __init__(self, width, height):
-        print "Loading Camera..."
-        pygame.camera.init()
-        cameras = pygame.camera.list_cameras()
-        count = 1
-        while len(cameras) < 1:
-            cameras = pygame.camera.list_cameras()
-            count += 1
-        
-        if len(cameras) < 1:
-           print "Unable to load camera... Exiting!"
-           sys.exit()
-        else:
-            print "Camera {0} loaded in {1} attempts.".format(cameras[0], count)
-            
+    def __init__(self, camera, width, height, id):          
         self.width = width
         self.height = height
-        self.webcam = pygame.camera.Camera(cameras[0], (self.width, self.height), "RGB")
+        self.id = id
+        self.webcam = pygame.camera.Camera(camera, (self.width, self.height), "RGB")
         self.webcam.start()
     
     def get_dimensions(self):
@@ -36,30 +24,35 @@ class Camera:
         
     def start_display(self):
         self.screen = pygame.display.set_mode((self.width, self.height))
-        pygame.display.set_caption("Brook Camera View")
+        pygame.display.set_caption("Camera{0}: Brook Camera View".format(self.id))
         
     def refresh_display(self):
         # grab next frame
         img = self.webcam.get_image()
+        
         # draw frame
         self.screen.blit(img, (0,0))
         pygame.display.flip()
     
     def stop(self):
-        print "Exiting..."
+        print "Camera{0}: Exiting...".format(self.id)
         pygame.display.quit()
         self.webcam.stop()
 
 class CameraRunner:
     
-    def __init__(self, id):
-        #id: int
+    def __init__(self, id, camera):
+        print "Runner{0}: Loading camera {1}".format(id, camera)
         width = 640
         height = 480
-        self.cam = Camera(width, height)
+        self.cam = Camera(camera, width, height, id)
         print "Runner{0}: Stream dimensions are {1}.".format(id, self.cam.get_dimensions())
+        self.process = Process(target=self.refresh, args=())
     
     def start(self):
+        self.process.start()
+    
+    def refresh(self):
         self.cam.start_display()
         while True :
             for e in pygame.event.get() :
@@ -69,15 +62,35 @@ class CameraRunner:
             
             self.cam.refresh_display()
         
+def LoadCameras(count):
+    print "Main: Loading Camera..."
+    pygame.camera.init()
+    cameras = pygame.camera.list_cameras()
+    count = 1
+    while len(cameras) < 2:
+        cameras = pygame.camera.list_cameras()
+        count += 1                                                
+    
+    if len(cameras) < 2:
+        print "Main: Unable to load camera... Exiting!"
+        sys.exit()
+    else:    
+        print "Main: Cameras {0} loaded in {1} attempts.".format(cameras, count)
+    
+    return cameras
+    
 if __name__ == "__main__":
     os.environ['PYGAME_CAMERA'] = 'opencv'
-    print "Starting Watcher..."
+    print "Main: Starting Watcher..."
+    
+    cameras = LoadCameras(2)
     
     # create a runner
-    runner1 = CameraRunner(1)
+    runner1 = CameraRunner(1, cameras[0])
+    runner2 = CameraRunner(2, cameras[1])
     
     # create a thread for the camera
     runner1.start()
-    
-    while True:
-        pass
+    runner2.start()
+    runner1.process.join()
+    runner2.process.join()
